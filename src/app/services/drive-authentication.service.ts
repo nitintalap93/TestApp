12d0dba205b6a52853d482ad8176ce72915d1fe1
Nitin Talap <nitin.rtalap@gmail.com>
2024-07-21 13:58:58 +0530

    Updated auth tokens

--- src/app/services/drive-authentication.service.ts
+++ src/app/services/drive-authentication.service.ts
@@ -7,12 +7,12 @@ import { Observable } from 'rxjs';
 })
 export class DriveAuthenticationService {
 
-  baseUrl: string = "https://www.googleapis.com/oauth2/v4/token";
+  baseUrl: string = "https://oauth2.googleapis.com/token";
   requestBody = {
     'client_id': '1060386232885-s75io32tnfseth2ncqgb1uga8u7u2ema.apps.googleusercontent.com',
     'client_secret': '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',
-    'refresh_token': '1//044iSnjXj_12TCgYIARAAGAQSNwF-L9IrU2CZwQpcNCbZXGd4c1ro9kAFyXClDoKQqYJ3r7oiYGgRGBJIUulARIZ6O5TRfehdxq4',
-    "grant_type": "refresh_token"
+    'refresh_token': '1//048rNBb0GIZCdCgYIARAAGAQSNwF-L9Ir9X348FyA1Zk43HlQN2DWKNnHrE3sUcV9SmDhu-CR-a1J-ji-qqCBN2rEdbaXK16Odto',
+    'grant_type': 'refresh_token'
   }
 
   constructor(private http: HttpClient) { }
